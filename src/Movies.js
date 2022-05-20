040d0774498b723bacda7ae385585a54711d7793
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-19 22:21:10 -0700

    ✔ Done setting show movies

--- src/Movies.js
+++ src/Movies.js
@@ -1,11 +1,40 @@
-import React from 'react'
-import { useGlobalContext } from './context'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { useGlobalContext } from './context';
+import { Link } from 'react-router-dom';
 const url =
-  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+   'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png';
 
 const Movies = () => {
-  return <h2>movies component</h2>
-}
+   const { movies, loading } = useGlobalContext();
 
-export default Movies
+   if (loading) {
+      return <div className="loading"> LOADING</div>;
+   }
+   return (
+      <section className="movies">
+         {/* deconstruct  */}
+         {movies.map((movie) => {
+            const {
+               imdbID: id,
+               Poster: poster,
+               Title: title,
+               Year: year,
+            } = movie;
+            return (
+               <Link to={`/movies/${id}`} key={id} className="movie">
+                  <article>
+                     {/* set default poster is not showing  */}
+                     <img src={poster === 'N/A' ? url : poster} alt={title} />
+                     <div className="movie-info">
+                        <h4 className="title">{title}</h4>
+                        <p>{year}</p>
+                     </div>
+                  </article>
+               </Link>
+            );
+         })}
+      </section>
+   );
+};
+
+export default Movies;
